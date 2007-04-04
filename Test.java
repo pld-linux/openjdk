5e5c90ed9e7ae98baab452165d9bff676d3256fc
Elan Ruusamäe <glen@pld-linux.org>
2007-04-04 11:15:26 +0000

    - class to test class version
    Changed files:
        Test.java -> 1.1

---
+++ Test.java
@@ -0,0 +1,9 @@
+/**
+ * Class for testing Class Version this JVM is able to load
+ * We test this by compiling class and looking it's version.
+ */
+
+public class Test {
+    public static void main(String[] args) {
+	}
+}
